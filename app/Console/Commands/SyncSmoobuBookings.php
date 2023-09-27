ac869b6eecb73e62d5d39efa3a0c508bf50afcc1
Jasper Cairoane Poly <ceejay@Jaspers-MacBook-Air.local>
2023-09-27 17:47:32 +0800

    Booking variable log

--- app/Console/Commands/SyncSmoobuBookings.php
+++ app/Console/Commands/SyncSmoobuBookings.php
@@ -41,7 +41,7 @@ class SyncSmoobuBookings extends Command
             'Cache-Control' => 'no-cache'
         ])->get('https://login.smoobu.com/api/reservations');
 
-        dd($bookings['bookings']);
+        dd($bookings);
 
         return false;
 
