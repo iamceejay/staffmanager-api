f4f6ec5d980992c5ff9ce6cacc55d5770f6d21d9
Jasper Cairoane Poly <ceejay@Jaspers-MacBook-Air.local>
2023-10-11 17:08:35 +0800

    Storage link typo error

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('invoice') => storage_path('app/invoice-temp'),
+        public_path('invoice') => storage_path('app/invoices-temp'),
     ],
 
 ];
