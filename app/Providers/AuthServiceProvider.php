904eb461c5cabf50c00f48fe466640f3b872a7cd
Jasper Cairoane Poly <ceejay@Jaspers-MacBook-Air.local>
2023-11-24 00:10:50 +0800

    Passport expiration

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,6 +23,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
+        Passport::tokensExpireIn(now()->addMinutes(5));
         
         // Passport::routes();
     }
