377620f3c5cf2cfb4aba125398ec04f1ce9ecfc2
iamceejay <jaspercairoane.poly@gmail.com>
2024-03-06 11:03:16 +0800

    Remove blocked bookings

---
+++ app/Console/Commands/RemoveBlockedBookings.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\SmoobuJob;
+
+class RemoveBlockedBookings extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'staffmanager:remove-blocked-bookings';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean up blocked bookings.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $key = getenv('SMOOBU_KEY');
+
+        $jobs = SmoobuJob::get();
+
+        foreach($jobs as $job) {
+            $booking = Http::acceptJson()->withHeaders([
+                'Api-Key'       => $key,
+                'Cache-Control' => 'no-cache'
+            ])->get('https://login.smoobu.com/api/reservations/' . $job->smoobu_id);
+
+            if($booking['is-blocked-booking']) {
+                SmoobuJob::where('smoobu_id', $job->smoobu_id)->delete();
+                echo 'Deleted Job with smoobu_id ' . $job->smoobu_id;
+            }
+        }
+    }
+}
