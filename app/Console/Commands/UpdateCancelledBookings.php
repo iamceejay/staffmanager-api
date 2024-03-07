8aa4bce71bf1eebde8bb8fef9a20ec30861c4e7e
iamceejay <jaspercairoane.poly@gmail.com>
2024-03-07 10:44:07 +0800

    Update script

--- app/Console/Commands/UpdateCancelledBookings.php
+++ app/Console/Commands/UpdateCancelledBookings.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\SmoobuJob;
+use Illuminate\Support\Facades\Http;
 
 class UpdateCancelledBookings extends Command
 {
@@ -11,20 +13,43 @@ class UpdateCancelledBookings extends Command
      *
      * @var string
      */
-    protected $signature = 'app:update-cancelled-bookings';
+    protected $signature = 'staffmanager:update-cancelled-bookings';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Updated status of jobs that were canceled on Smoobu.';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $key = getenv('SMOOBU_KEY');
+
+        $jobs = SmoobuJob::get();
+
+        foreach($jobs as $job) {
+            echo "Smoobu ID:  $job->smoobu_id \r\n";
+
+            $booking = Http::acceptJson()->withHeaders([
+                'Api-Key'       => $key,
+                'Cache-Control' => 'no-cache'
+            ])->get('https://login.smoobu.com/api/reservations/' . $job->smoobu_id);
+
+            if($booking->getStatusCode() === 404) {
+                continue;
+            }
+
+            if($booking['type'] === 'cancellation') {
+                SmoobuJob::where('smoobu_id', $job->smoobu_id)->update([
+                    'status' => 'cancelled'
+                ]);
+
+                echo "Updated Job with smoobu_id $job->smoobu_id \r\n";
+            }
+        }
     }
 }
